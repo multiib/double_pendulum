362adcdb0ea444af48c1e4469abb9fc22487fc37
Benjamin Borge <multiib@icloud.com>
2021-09-21 23:54:00 +0200

    Finished up task 1

--- exp_decay.py
+++ exp_decay.py
@@ -3,12 +3,9 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 class ExponentialDecay:
-    """
-    ODE class for exponential decay.
-    """
+    """ ODE class for exponential decay """
+
     def __init__(self, a):
         self.a = a
         if a < 0:
@@ -17,22 +14,21 @@ class ExponentialDecay:
             pass
 
     def __call__(self, t, u):
-
-
         return -self.a*u
 
     def solve(self, U0, T, dt):
-        t_ev = np.linspace(0, T, int(T/dt)) # setting up dt evaluation
+        """Method for solving the ODE with initial conditions"""
+
+        t_ev = np.linspace(0, T, int(T/dt) + 1) # setting up dt evaluation
 
         sol = solve_ivp(self.__call__, [0, T], [U0], t_eval=t_ev)
         return sol.t, sol.y[0]
 
 
-
 if __name__ == "__main__":
-
+    """Run example"""
+    
     decay_model = ExponentialDecay(2)
-    t, u= decay_model.solve(1, 7, 0.001)
-    print(len)
+    t, u = decay_model.solve(1, 7, 0.001)
     plt.plot(t, u)
     plt.show()
--- test_exp_decay.py
+++ test_exp_decay.py
@@ -1,8 +1,6 @@
 from exp_decay import *
 import pytest
 
-
-
 def test_ExponentialDecay_a_is_positive():
     """
     Testing that class ExponentialDecay can't take in a negative number.
@@ -11,9 +9,28 @@ def test_ExponentialDecay_a_is_positive():
     with pytest.raises(ValueError):
         negative_instance = ExponentialDecay(-1)
 
+
 def test_ExponentialDecay_correct_calculation():
     """
     Testing correct calculation in ODE
     """
     calc_instance = ExponentialDecay(0.4)
     assert calc_instance(1, 3.2) == pytest.approx(-1.28)
+
+
+dt = 0.00001
+@pytest.mark.parametrize(
+    "t_, out", [(1/dt + 1, 0.5413411329), (2/dt + 1, 0.0732625556), (3/dt + 1, 0.0099150087)]
+)
+
+def test_ExponentialDecay_solve(t_, out):
+    """
+    Testing that the solve method is implemented correctly.
+    """
+
+    allowed_error = 1e-03
+    decay_model = ExponentialDecay(2)
+
+    t, u = decay_model.solve(4, 5, dt)
+
+    assert abs(u[int(t_) +1] - out) <= allowed_error
