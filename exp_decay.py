dcac52cb547196bfa15024f8844fd51c8d0d78c0
Benjamin Borge <multiib@icloud.com>
2021-09-21 14:03:30 +0200

    Started on 1c, add solve_ivp

--- exp_decay.py
+++ exp_decay.py
@@ -1,4 +1,9 @@
 import scipy
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 
 class ExponentialDecay:
     """
@@ -12,5 +17,22 @@ class ExponentialDecay:
             pass
 
     def __call__(self, t, u):
-        a = self.a
-        return -a*u
+
+
+        return -self.a*u
+
+    def solve(self, U0, T, dt):
+        t_ev = np.linspace(0, T, int(T/dt)) # setting up dt evaluation
+
+        sol = solve_ivp(self.__call__, [0, T], [U0], t_eval=t_ev)
+        return sol.t, sol.y[0]
+
+
+
+if __name__ == "__main__":
+
+    decay_model = ExponentialDecay(2)
+    t, u= decay_model.solve(1, 7, 0.001)
+    print(len)
+    plt.plot(t, u)
+    plt.show()
